4e170da210b003663729cb8307e704c6a273c309
mole99 <leomoser99@gmail.com>
2022-06-15 09:31:32 +0200

    Sync with waveform-generator

--- verilog/rtl/wfg/wfg_top/testbench/test_wfg_top.py
+++ verilog/rtl/wfg/wfg_top/testbench/test_wfg_top.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: © 2022 semify <office@semify-eda.com>
 # SPDX-License-Identifier: Apache-2.0
 
-import random
+import random, math, statistics
 import matplotlib.pyplot as plt
 from scipy import optimize
 import numpy as np
@@ -16,7 +16,6 @@ from cocotbext.spi import SpiMaster, SpiSignals, SpiConfig, SpiSlaveBase
 
 CLK_PER_SYNC = 300
 SYSCLK = 100000000
-DATA_CNT = 10
 
 short_per = Timer(100, units="ns")
 long_time = Timer(100, units="us")
@@ -63,11 +62,9 @@ async def set_register(dut, wbs, peripheral_address, address, data):
         dut._log.error("Can not access peripheral registers outside 0xF")
 
     real_address = (peripheral_address<<4) | (address & 0xF)
-
     dut._log.info(f"Set register {real_address} : {data}")
 
     wbRes = await wbs.send_cycle([WBOp(real_address, data)])
-    
     rvalues = [wb.datrd for wb in wbRes]
     dut._log.info(f"Returned values : {rvalues}")
 
@@ -75,7 +72,10 @@ async def configure_core(dut, wbs, en, sync_count, subcycle_count):
     await set_register(dut, wbs, 0x1, 0x4, (sync_count << 0) | (subcycle_count << 8))
     await set_register(dut, wbs, 0x1, 0x0, en) # Enable
 
-async def configure_stim_sine(dut, wbs, en):
+async def configure_stim_sine(dut, wbs, en, inc=0x1000, gain=0x4000, offset=0):
+    await set_register(dut, wbs, 0x2, 0x4, inc)
+    await set_register(dut, wbs, 0x2, 0x8, gain)
+    await set_register(dut, wbs, 0x2, 0xC, offset)
     await set_register(dut, wbs, 0x2, 0x0, en) # Enable
 
 async def configure_drive_spi(dut, wbs, en=1, cnt=3, cpol=0, lsbfirst=0, dff=0, sspol=0):
@@ -100,7 +100,7 @@ async def top_test(dut):
     wbs = WishboneMaster(dut, "io_wbs", dut.io_wbs_clk,
                               width=32,   # size of data bus
                               timeout=10) # in clock cycle number
-
+    # SPI settings
     dff = 3
     cnt = 3
     cpol = 0
@@ -129,45 +129,71 @@ async def top_test(dut):
     )
     
     spi_slave = SimpleSpiSlave(dut, spi_signals, spi_config)
+    
+    # Sine settings
+    sine_inc = 0x1000
+    sine_gain = 0x4000
+    sine_offset = 0
+    
+    num_spi_values = int((2**16) / sine_inc + 1)
 
     # Setup core
     dut._log.info("Configure core")
     await configure_core(dut, wbs, en=1, sync_count=16, subcycle_count=16)
     dut._log.info("Configure stim_sine")
-    await configure_stim_sine(dut, wbs, en=1)
+    await configure_stim_sine(dut, wbs, en=1, inc=sine_inc, gain=sine_gain, offset=sine_offset)
     dut._log.info("Configure drive_spi")
     await configure_drive_spi(dut, wbs, en=1, cnt=cnt, cpol=cpol, lsbfirst=lsbfirst, dff=dff, sspol=sspol)
 
-    await long_time
-    await short_per
+    while len(spi_slave.values) < num_spi_values:
+        await short_per
 
     def test_func(x, a, b, c):
         return a * np.sin(b * x + c)
 
     params, params_covariance = optimize.curve_fit(test_func, spi_slave.time, spi_slave.values, p0=[70000, 0.00007, -0.5])
     
-    print(params)
-    print(spi_slave.time)
-    print(spi_slave.values)
-    
-    # Loosely check parameters
-    assert(abs(params[0] - 65532.57101443629) < 0.1)
-    assert(abs(params[1] - 6.79379545e-05) < 0.0001)
-    assert(abs(params[2] - -1.86659076e-01) < 0.001)
+    dut._log.info(params)
+    dut._log.info(spi_slave.time)
+    dut._log.info(spi_slave.values)
 
     x_data = np.asarray(spi_slave.time)
     y_data = np.asarray(spi_slave.values)
-    y_calc = test_func(x_data, params[0], params[1], params[2])
-    y_error = (y_data - y_calc)
     
+    #y_calc = test_func(x_data, params[0], params[1], params[2])
+    
+    y_error = []
+    y_squared_error = []
+    y_absolute_error = []
+    y_data_float = []
+    
+    for (cnt, value) in enumerate(y_data):
+        input_val = cnt * sine_inc
+        while input_val >= (2**16):
+            input_val -= (2**16)
+    
+        angle_rad = float(input_val) / (2**16) * 2 * math.pi
+        calculated_value = math.sin(angle_rad) * (sine_gain / 2**14) + (sine_offset/2**16)
+        output_as_float = float(value) / (2**16)
+        y_data_float.append(output_as_float)
+        y_error.append(output_as_float - calculated_value)
+        y_squared_error.append((output_as_float - calculated_value)**2)
+        y_absolute_error.append(abs(output_as_float - calculated_value))
+    
+    y_mean_squared_error = statistics.mean(y_squared_error)
+    dut._log.info("y_mean_squared_error: {}".format(y_mean_squared_error))
+    
+    y_mean_absolute_error = statistics.mean(y_absolute_error)
+    dut._log.info("y_mean_absolute_error: {}".format(y_mean_absolute_error))
+
     x_data_highres = np.linspace(x_data[0], x_data[-1], num=100)
     y_calc_highres = test_func(x_data_highres, params[0], params[1], params[2])
     
     fig, ax = plt.subplots(2, 1)
-    fig.suptitle('Stimuli: Sine, Driver: SPI', fontsize=16)
+    fig.suptitle('Sine Stimuli and SPI Driver', fontsize=16)
     
-    ax[0].scatter(x_data, y_data, label='SPI data')
-    ax[0].plot(x_data_highres, y_calc_highres, label='Fitted function')
+    ax[0].scatter(x_data, y_data_float, label='SPI data as float')
+    #ax[0].plot(x_data_highres, y_calc_highres, label='Fitted function')
     ax[0].set(xlabel='time in ns', ylabel='value')
     ax[0].legend(loc='best')
     ax[0].grid()
@@ -177,6 +203,11 @@ async def top_test(dut):
     ax[1].legend(loc='best')
     ax[1].grid()
 
-    fig.savefig("output.png")
+    figure = plt.gcf()
+    figure.set_size_inches(10, 6)
+    plt.tight_layout()
+    fig.savefig("output_inc={}_gain={}_off={}.svg".format(sine_inc, sine_gain, sine_offset))
+    fig.savefig("output_inc={}_gain={}_off={}.png".format(sine_inc, sine_gain, sine_offset), dpi=199)
     #plt.show()
-
+    
+    assert(y_mean_absolute_error < 0.001)
