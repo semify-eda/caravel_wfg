3ef53462c4f809e26da8628a1f7add60bb520e94
mole99 <leomoser99@gmail.com>
2022-07-07 08:40:25 +0200

    Start with wishbone mux

---
+++ verilog/rtl/wb_memory.sv
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: © 2022 semify <office@semify-eda.com>
+// SPDX-License-Identifier: Apache-2.0
+
+`default_nettype none
+module wb_memory (
+`ifdef USE_POWER_PINS
+    inout vccd1,	// User area 1 1.8V supply
+    inout vssd1,	// User area 1 digital ground
+`endif
+
+    input test,
+    output test2
+);
+
+    assign test2 = test;
+
+endmodule
+`default_nettype wire
